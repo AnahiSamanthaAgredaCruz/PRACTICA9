1b3f8cfc72e69419afa3628586c4d234e0de4ab5
Anahi Samantha Agreda Cruz <a2019118777@estudiantes.upsa.edu.bo>
2020-12-12 01:00:22 -0400

    se creo el controlador

---
+++ Final/Final/Controllers/OperacionController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Final.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperacionController : ControllerBase
+    {
+
+        [HttpGet]
+        public string operacion (int numero)
+        {
+            string respuesta = "";
+            if(numero < 0)
+            { respuesta = "ERROR";
+            }
+            if (numero == 0)
+            {
+                respuesta = "Realizado por Anahi Agreda";
+            }
+            if (numero > 0)
+            {
+                respuesta = "Usted ingreso el número";
+            }
+
+            return respuesta;
+        }
+
+    }
+}
